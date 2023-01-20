6b58cc7b0cd8fcc913691c3b660c97beb1cf22ba
Aaron Defazio <adefazio@fb.com>
2023-01-20 07:40:47 -0800

    Packaging fixes

---
+++ dadaptation/__init__.py
@@ -0,0 +1,3 @@
+from dadapt_adagrad import DAdaptAdaGrad
+from dadapt_adam import DAdaptAdam
+from dadapt_sgd import DadaptSGD
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dadaptation",
-    version="1.01",
+    version="1.1",
     author="Aaron Defazio",
     author_email="adefazio@meta.com",
     description="Learning Rate Free Learning for Adam, SGD and AdaGrad",
